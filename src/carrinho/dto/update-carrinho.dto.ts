b8cdae613284a7ff242508bea69c035e68348dd2
camilapessoa <camilabentespessoa@gmail.com>
2023-05-08 17:01:55 -0300

    update

--- src/carrinho/dto/update-carrinho.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCarrinhoDto } from './create-carrinho.dto';
-
-export class UpdateCarrinhoDto extends PartialType(CreateCarrinhoDto) {}
